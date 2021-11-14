e6eea95957b929908f6e540e3f50a308063f70b9
ravishukla86044 <ravishukls86044@gmail.com>
2021-11-14 12:55:05 +0530

    added router

--- gooderafrontend/src/App.jsx
+++ gooderafrontend/src/App.jsx
@@ -2,12 +2,29 @@ import logo from "./logo.svg";
 import { Header } from "./Components/Header/Header";
 import "./App.css";
 import { Search } from "./Components/Middle/Search";
+import { Switch, Route, useLocation } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Search />
+      <Switch>
+        <Route exact path="/">
+          <>
+            <Search />
+          </>
+        </Route>
+
+        <Route path="/upskill">
+          <h1>Upskill Yourself Here</h1>
+        </Route>
+        <Route path="/postjob">
+          <h1>Post a Job here</h1>
+        </Route>
+        <Route path="/login">
+          <h1>Login Here</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
