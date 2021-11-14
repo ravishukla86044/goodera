cc0d61eb868fabcb07783004f5f3d04382ba0cd4
ravishukla86044 <ravishukls86044@gmail.com>
2021-11-14 13:58:21 +0530

    added more date

--- backend/src/server.js
+++ backend/src/server.js
@@ -43,6 +43,42 @@ let data = [
       name: "Amazon",
     },
   },
+  {
+    description:
+      "Experience in Cloud Computing technologies, scripting languages (JSON, Python, RoR, etc), integrating 3rd party monitoring tools, encryption tools, and forensics. Implementation experience with enterprise security solutions such as WAF, IPS, Anti-DDOS, and SIEM.",
+    title: "IT",
+    image:
+      "https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2940&q=80",
+    type: "external",
+    publication_date: "2021-08-04T00:04:04Z",
+    id: 5690411,
+    location: "London",
+    level: "senior",
+    tags: [],
+    company: {
+      id: 12090,
+      short_name: "amazon",
+      name: "Amazon",
+    },
+  },
+  {
+    description:
+      "Experience in Cloud Computing technologies, scripting languages (JSON, Python, RoR, etc), integrating 3rd party monitoring tools, encryption tools, and forensics. Implementation experience with enterprise security solutions such as WAF, IPS, Anti-DDOS, and SIEM.",
+    title: "SDE",
+    image:
+      "https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2940&q=80",
+    type: "external",
+    publication_date: "2021-08-04T00:04:04Z",
+    id: 5690411,
+    location: "India",
+    level: "senior",
+    tags: [],
+    company: {
+      id: 12090,
+      short_name: "amazon",
+      name: "Amazon",
+    },
+  },
 ];
 
 app.get("/jobs", function (req, res) {
